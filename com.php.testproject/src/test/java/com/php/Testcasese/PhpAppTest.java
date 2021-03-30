fb95cc32c268e243544299dea668f97a9603f40b
DELL <DELL@DELL-PC>
2021-03-30 10:40:53 +0530

    php first commit

---
+++ com.php.testproject/src/test/java/com/php/Testcasese/PhpAppTest.java
@@ -0,0 +1,16 @@
+package com.php.Testcasese;
+
+import org.testng.annotations.Test;
+
+public class PhpAppTest 
+{
+  
+    @Test
+    public void validlogin()
+    {
+      System.out.println("php project");
+      System.out.println("test project");
+      System.out.println("jenkins project");
+      System.out.println("git project");
+    }
+}
